b51386dce036008096739fb82269d4110f0ac0a5
nilipta <niliptasatapathy@gmail.com>
2021-05-02 00:27:03 +0530

    added html form to python receive POST data

--- owner/urls.py
+++ owner/urls.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 
-from .views import Display
+from .views import Display, AddFormClass
 
 
 urlpatterns = [
     path('', Display.homeDisplay),
+    path('add-item', AddFormClass.get_name),
 ]
--- owner/views.py
+++ owner/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
 from django.views import View
+from django.db import models
+from django.forms import ModelForm
 
 from .models import Item    
 
@@ -11,6 +13,32 @@ class Display(View):
     def homeDisplay(request):
         context= {
             'page_name': "Home page",
-            "items": Item.objects.all()[:5]
+            "items": Item.objects.all()
         }
         return render(request, 'owner/index.html', context)
+
+
+class ItemForm(ModelForm):
+    class Meta:
+        model = Item
+        fields = ['timeStamp', 'name', 'price', 'visibleToCostumer', 'profitPercentage', 'item_image' ]
+
+
+class AddFormClass:
+    def get_name(request):
+        # if this is a POST request we need to process the form data
+        if request.method == 'POST':
+            # create a form instance and populate it with data from the request:
+            form = ItemForm(request.POST)
+            # check whether it's valid:
+            if form.is_valid():
+                # process the data in form.cleaned_data as required
+                print("Item details = ", form.cleaned_data)
+                # redirect to a new URL:
+                return HttpResponse('/thanks/')
+
+        # if a GET (or any other method) we'll create a blank form
+        else:
+            form = ItemForm()
+
+        return render(request, 'owner/addItem.html', {'form': form})
